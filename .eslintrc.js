e4510f1b35f47946343e720d831c4adb0e11e1fc
kaoru.ohtaka <ockacru+git@gmail.com>
2022-02-03 21:35:47 +0900

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,40 @@
 /** @type import('eslint').Linter.BaseConfig */
-module.exports = { extends: 'next/core-web-vitals' };
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'next/core-web-vitals',
+    'plugin:jest/recommended',
+    // 'prettier' needs to be bottom
+    'prettier',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'max-lines': 'error',
+    'max-depth': 'error',
+    'max-lines-per-function': 'error',
+    'react/jsx-props-no-spreading': 'off',
+  },
+  env: {
+    browser: true,
+    node: true,
+    'jest/globals': true,
+  },
+  overrides: [
+    {
+      files: ['pages/*.page.tsx'],
+      rules: {
+        'react/function-component-definition': 'off',
+      },
+    },
+  ],
+};
