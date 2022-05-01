38912865855ac03826efa83d49f6bcfcec027ebe
kaoru.ohtaka <ockacru+git@gmail.com>
2022-05-02 08:51:03 +0900

    Define global styles in emotion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { GlobalStyles } from '../styles/global';
+
+import 'ress';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
