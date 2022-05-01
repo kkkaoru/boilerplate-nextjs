d9d2341a4190777337e06345d7824a00ecbf1831
kaoru.ohtaka <ockacru+git@gmail.com>
2022-05-02 08:22:18 +0900

    Can commit with using emotion

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@
 module.exports = {
   extends: [
     'airbnb',
-    'airbnb/hooks',
     'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
